9372553d4120b16244d6e3e02f9a3dd942e108e6
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2022-07-04 02:17:22 +0000

    adding back in UserWarning to connection test

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -17,13 +17,14 @@ def test_connection_with(sftpserver):
 def test_connection_bad_host():
     '''attempt connection to a non-existing server'''
     with pytest.raises(AuthenticationException):
-       cnopts = CnOpts()
-       cnopts.hostkeys = None
-       sftp = Connection(host='',
-                         username='demo',
-                         password='password',
-                         cnopts=cnopts)
-       sftp.close()
+        with pytest.raises(UserWarning):
+            cnopts = CnOpts()
+            cnopts.hostkeys = None
+            sftp = Connection(host='',
+                              username='demo',
+                              password='password',
+                              cnopts=cnopts)
+            sftp.close()
 
 
 @SKIP_IF_CI
