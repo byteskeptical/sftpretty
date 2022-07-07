e83ebec132b27af2b98ba1d40930c9db46459c63
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2022-07-07 18:07:59 +0000

    loco for nonlocal

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -17,10 +17,11 @@ class UnexpectedError(Exception):
 
 
 def test_no_retry_required():
+    counter=0
 
     @retry(RetryableError, tries=4, delay=0.1)
-    def succeeds(counter=0):
-        counter += 1
+    def succeeds():
+        nonlocal counter += 1
         return 'success'
 
     r = succeeds()
@@ -29,10 +30,11 @@ def test_no_retry_required():
     assert counter == 1
 
 def test_retries_once():
+    counter=0
 
     @retry(RetryableError, tries=4, delay=0.1)
-    def fails_once(counter=0):
-        counter += 1
+    def fails_once():
+        nonlocal counter += 1
         if counter < 2:
             raise RetryableError('failed')
         else:
@@ -44,10 +46,11 @@ def test_retries_once():
     assert counter == 2
 
 def test_limit_is_reached():
+    counter=0
 
     @retry(RetryableError, tries=4, delay=0.1)
-    def always_fails(counter=0):
-        counter += 1
+    def always_fails():
+        nonlocal counter += 1
         raise RetryableError('failed')
 
     with pytest.raises(RetryableError, match='failed'):
@@ -56,10 +59,11 @@ def test_limit_is_reached():
     assert counter == 4
 
 def test_multiple_exception_types():
+    counter=0
 
     @retry((RetryableError, AnotherRetryableError), tries=4, delay=0.1)
-    def raise_multiple_exceptions(counter=0):
-        counter += 1
+    def raise_multiple_exceptions():
+        nonlocal counter += 1
         if counter == 1:
             raise RetryableError('a retryable error')
         elif counter == 2:
@@ -84,6 +88,7 @@ def test_unexpected_exception_does_not_retry():
 @pytest.fixture(autouse=True)
 def test_using_a_logger(caplog):
     _caplog = caplog
+    counter = 0
     expected = {'DEBUG': 'success',
                 'ERROR': 'failed',
                 'WARNING': ('Retry (4/4):\nfailed\nRetrying in 0.1 '
@@ -95,8 +100,8 @@ def test_using_a_logger(caplog):
     log.addHandler(sh)
 
     @retry(RetryableError, tries=4, delay=0.1, logger=log)
-    def fails_once(counter=0):
-        counter += 1
+    def fails_once():
+        nonlocal counter += 1
         if counter < 2:
             log.error('failed')
             raise RetryableError('failed')
