4e9e0d33f8c5dfd88aa1a8f1e1b046bee6a1c2b2
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2023-03-12 05:34:18 +0000

    adding cnopts to lsftp function for certain local tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,14 @@
 import pytest
 
 from common import SFTP_LOCAL
-from sftpretty import Connection
+from sftpretty import CnOpts, Connection
 
 
 @pytest.fixture(scope="session")
 def lsftp(request):
     '''setup a session long connection to the local sftp server'''
+    cnopts = CnOpts(knownhosts='sftpserver.pub')
+    SFTP_LOCAL.cnopts = cnopts
     lsftp = Connection(**SFTP_LOCAL)
     request.addfinalizer(lsftp.close)
     return lsftp  # provide the fixture value
