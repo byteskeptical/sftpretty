71c04e61eba30077e0362c608983b420401fa178
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2023-03-12 05:50:05 +0000

    switching host key checking off for local tests, bringing passwords to parity for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from sftpretty import CnOpts, Connection
 @pytest.fixture(scope="session")
 def lsftp(request):
     '''setup a session long connection to the local sftp server'''
-    cnopts = CnOpts(knownhosts='sftpserver.pub')
+    cnopts = CnOpts(knownhosts=None)
     SFTP_LOCAL['cnopts'] = cnopts
     lsftp = Connection(**SFTP_LOCAL)
     request.addfinalizer(lsftp.close)
