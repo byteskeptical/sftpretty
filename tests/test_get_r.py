f95c7065b090a884d4b0d165d9c7a84c1540845f
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2023-04-13 23:36:53 +0000

    that got worse, didn't wrap in Path()

--- tests/test_get_r.py
+++ tests/test_get_r.py
@@ -19,7 +19,7 @@ def test_get_r(sftpserver):
             remote_cwd = sftp.pwd
             local_cwd = Path(localpath).joinpath(
                              remote_cwd.lstrip('/')).as_posix()
-            cwd = [b.as_posix() for b in local_cwd.iterdir()] 
+            cwd = [b.as_posix() for b in Path(local_cwd).iterdir()] 
 
             localtree(local_tree, local_cwd, localpath)
             sftp.remotetree(remote_tree, remote_cwd, localpath)
