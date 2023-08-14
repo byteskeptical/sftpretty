8b285f2f8e9f78c0bba8d2fa1ca6461f238d589c
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2023-08-14 02:34:11 +0000

    making sure to connect to the sftp test server by using 127.0.0.1 ip and matching ssh config host for tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,13 +11,12 @@ def test_connection_with_config(sftpserver):
     config = Path(f'{USER_HOME}/.ssh/config')
     config.parent.mkdir(exist_ok=True, mode=0o700)
     config.touch(exist_ok=True, mode=0o644)
-    config.write_bytes(bytes(('Host localhost\n\t'
+    config.write_bytes(bytes((f'Host 127.0.0.1\n\t'
                               f'User {USER}').encode('utf-8')))
-    cnopts = CnOpts(config=config.as_posix(),
-                    knownhosts='sftpserver.pub')
+    cnopts = CnOpts(config=config.as_posix(), knownhosts='sftpserver.pub')
     with sftpserver.serve_content(VFS):
-        with Connection('localhost', cnopts=cnopts, default_path='/home/test',
-                        password=PASS) as sftp:
+        with Connection(sftpserver.host, cnopts=cnopts,
+                        default_path='/home/test', password=PASS) as sftp:
             assert sftp.listdir() == ['pub', 'read.me']
 
 
@@ -27,11 +26,10 @@ def test_connection_with_config_alias(sftpserver):
     config.parent.mkdir(exist_ok=True, mode=0o700)
     config.touch(exist_ok=True, mode=0o644)
     config.write_bytes(bytes(('Host test\n\t'
-                              'Hostname localhost\n\t'
+                              'Hostname 127.0.0.1\n\t'
                               'IdentityFile id_sftpretty\n\t'
                               f'User {USER}').encode('utf-8')))
-    cnopts = CnOpts(config=config.as_posix(),
-                    knownhosts='sftpserver.pub')
+    cnopts = CnOpts(config=config.as_posix(), knownhosts='sftpserver.pub')
     with sftpserver.serve_content(VFS):
         with Connection('test', cnopts=cnopts, default_path='/home/test',
                         private_key_pass=PASS) as sftp:
@@ -43,12 +41,12 @@ def test_connection_with_config_identity(sftpserver):
     config = Path(f'{USER_HOME}/.ssh/config')
     config.parent.mkdir(exist_ok=True, mode=0o700)
     config.touch(exist_ok=True, mode=0o644)
-    config.write_bytes(bytes(('Host localhost\n\t'
+    config.write_bytes(bytes(('Host 127.0.0.1\n\t'
                               'IdentityFile id_sftpretty\n\t'
                               f'User {USER}').encode('utf-8')))
-    cnopts = CnOpts(config=config.as_posix(),
-                    knownhosts='sftpserver.pub')
+    cnopts = CnOpts(config=config.as_posix(), knownhosts='sftpserver.pub')
     with sftpserver.serve_content(VFS):
-        with Connection('localhost', cnopts=cnopts, default_path='/home/test',
+        with Connection(sftpserver.host, cnopts=cnopts,
+                        default_path='/home/test',
                         private_key_pass=PASS) as sftp:
             assert sftp.listdir() == ['pub', 'read.me']
