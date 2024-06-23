dcb826d3d7fee43ab5ec484bd439d70a3b01bbc3
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2024-06-23 14:22:28 +0000

    how did this one get away

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -6,7 +6,7 @@ from pep8 import StyleGuide
 def test_pep8():
     '''pep8 check the source'''
     # list the specific files or directories to check, directories are recursed
-    paths = ['sftpretty', 'setup.py', 'tests']
+    paths = ['sftpretty', 'pyproject.toml', 'tests']
     p8c = StyleGuide()
     report = p8c.check_files(paths=paths)
     report.print_statistics()
