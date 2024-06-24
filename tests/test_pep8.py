63509f97c7ce29803caee2a533507f1f9b1e18c4
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2024-06-24 00:06:54 +0000

    pep8 has changed

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -6,7 +6,7 @@ from pep8 import StyleGuide
 def test_pep8():
     '''pep8 check the source'''
     # list the specific files or directories to check, directories are recursed
-    paths = ['sftpretty', 'pyproject.toml', 'tests']
+    paths = ['sftpretty', 'tests']
     p8c = StyleGuide(ignore=['E231'])
     report = p8c.check_files(paths=paths)
     report.print_statistics()
