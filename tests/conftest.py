18a04b9e84f6cf350ff10d32f2348140142968d4
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2025-02-21 04:52:39 +0000

    should have never doubted myself autouse is indeed needed, changing scope instead

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,20 +18,19 @@ def lsftp(request):
     return lsftp
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(autouse=True, scope='module')
 def knownhosts(sftpserver, key_type='ssh-ed25519'):
     '''setup host key for test server in local knownhosts'''
     if sftpserver.port != 22:
         host = f'[{sftpserver.host}]:{sftpserver.port}'
     else:
         host = sftpserver.host
-    hashed_host = HostKeys().hash_host(host)
+    host_hashed = HostKeys().hash_host(host)
     hostkey = \
         'AAAAC3NzaC1lZDI1NTE5AAAAIB0g3SG/bbyysJ7f0kqdoWMXhHxxFR7aLJYNIHO/MtsD'
     hostkeys = f'''\
-        {hashed_host} {key_type} {hostkey}
         {host} {key_type} {hostkey}
-    '''
+        {host_hashed} {key_type} {hostkey}'''
     knownhosts = Path('sftpserver.pub')
     knownhosts.write_bytes(bytes(hostkeys, 'utf-8'))
 
