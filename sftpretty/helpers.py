61e8d3e9e2ae4680fc1a69e395acaa64e789f1ad
byteskeptical <40208858+byteskeptical@users.noreply.github.com>
2026-02-10 08:05:42 +0000

    need additional shim in drivedrop to deal with os.path.isabs behavior change in python 3.13+, which paramiko uses in normalize calls

--- sftpretty/helpers.py
+++ sftpretty/helpers.py
@@ -23,6 +23,8 @@ def drivedrop(filepath):
                 *PureWindowsPath(filepath).parts[1:]).as_posix()
             filepath = filepath.encode('unicode_escape').decode()
             filepath = filepath.replace('\\', '/').replace('//', '/')
+        else if filepath.startswith('//'):
+            filepath = filepath.replace('//', '/')
 
     return filepath
 
